046391298be46be0af2fe11da2c246886dcd1a0d
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-06-23 16:04:51 +0200

    predispongo i collegamenti header per le varie pagine

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header class="container">
+
+  <a href="{{ route("home") }}"><img src="" alt="DC Logo">home</a>
+  
+  <nav>
+    <ul>
+      <li>
+        <a href="{{ route("caracters") }}">CHARACTERS</a>
+        <a href="{{ route("comics") }}">COMICS</a>
+        <a href="{{ route("movies") }}">MOVIES</a>
+        <a href="{{ route("games") }}">GAMES</a>
+        <a href="{{ route("collectibles") }}">COLLECTIBLES</a>
+        <a href="{{ route("videos") }}">VIDEOS</a>
+        <a href="{{ route("fans") }}">FANS</a>
+        <a href="{{ route("news") }}">NEWS</a>
+        <a href="{{ route("shop") }}">SHOP</a>
+      </li>
+    </ul>
+  </nav>
+
+</header>
